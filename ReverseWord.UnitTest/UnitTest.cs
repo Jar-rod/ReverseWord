7b6619d639c427fc54797b5b0e17c9cf09a05415
Jar-rod <jarrod.ramsaroop@gmail.com>
2023-04-12 08:33:05 +0200

    Initial commit

---
+++ ReverseWord.UnitTest/UnitTest.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using ReverseWord.Console;
+
+namespace ReverseWord.UnitTest
+{
+  public class ReverseWordUnitTest
+  {
+ 
+    [Theory]
+    [InlineData("this is a test", "test a is this")]
+    [InlineData("foobar", "foobar")]
+    [InlineData("all your base", "base your all")]
+    public void ReverseWord_GivenMoreThanOneWord_ThenReverseTheSentence(string actual, string expected)
+    {
+      var actualValue = ReverseWord.Console.ReverseWord.ReverseWords(actual).Trim();
+      
+      Assert.Equal(
+          expected,
+          actualValue
+      );
+    }
+
+    [Theory]
+    [InlineData("this is a test that should failed but we'll try our best to can it right", true)]
+    [InlineData("this should be validated", false)]
+    //[InlineData("all your base", "base your all")]
+    public void ReverseWord_GivenMoreThanOneWordOf25letter_ThenReturnAnError(string actual, bool expected)
+    {
+      var actualValue = !ReverseWord.Console.ReverseWord.ValidateLetterLength(actual);
+
+      Assert.Equal(
+          expected,
+          actualValue
+      );
+    }
+  }
+}
---
+++ ReverseWord/ReverseWord.cs
@@ -0,0 +1,76 @@
+﻿namespace ReverseWord.Console
+{
+  public class ReverseWord
+  {
+    static void Main()
+    {
+      System.Console.Write("Enter number of Cases: ");
+
+      int numberOfCases;
+
+      //Number must be valid
+      while (!int.TryParse(System.Console.ReadLine(), out numberOfCases))
+      {
+        System.Console.Clear();
+        System.Console.WriteLine("You have entered an invalid number!");
+        System.Console.Write("Enter number of Cases: ");
+      }
+
+      string[] sentences = new string[numberOfCases];
+
+      for (int i = 0; i < numberOfCases; i++)
+      {
+        System.Console.Write($"{i+1}) ");
+        sentences[i] = System.Console.ReadLine();
+
+        //1 < letters < 25
+        while (!ValidateLetterLength(sentences[i]))
+        {
+          System.Console.WriteLine($"Sentence must be greater than {Letters.min} or less than {Letters.max} letters!");
+          System.Console.Write($"{i+1}) ");
+          sentences[i] = System.Console.ReadLine();
+        }
+      }
+
+      for (int i = 0; i < numberOfCases; i++)
+      {
+        System.Console.WriteLine($"Case {i + 1}: {ReverseWords(sentences[i])}");
+      }
+    }
+
+    public static string ReverseWords(string sentence)
+    {
+      string[] words = sentence.Split(" ");
+      string[] reversedSentence = new string[words.Length];
+
+      for (int i = 0; i < words.Length; i++)
+      {
+        int index = words.Length - (i + 1);
+        reversedSentence[i] = words[index];
+      }
+
+      string output = "";
+      foreach (string word in reversedSentence)
+      {
+        output += $" {word}";
+      }
+
+      return output;
+    }
+
+    public static bool ValidateLetterLength(string sentence)
+    {
+      if (sentence.Replace(" ", "").Length >= (int)Letters.max || sentence.Replace(" ", "").Length <= (int)Letters.min)
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private enum Letters
+    {
+      min = 1,
+      max = 25
+    }
+  }
+}
